252ae41f20947b8b9c149b2ede131530f3bdab57
Allan Duarte Ehlert <everaldoallan@gmail.com>
2022-07-12 08:58:53 -0300

    Update g3zao.ino

--- g3zao.ino
+++ g3zao.ino
@@ -20,29 +20,21 @@ void loop() {
     Serial.println(results.value, HEX);
     armazenavalor = (results.value);
   }
-  if(results.value == 0xAF5E817){ //Botao OFF
-    Serial.println("Botao OFF");
+  if(results.value == 0xAF5E817 || results.value == 0xFF02FD){ //Botao OFF ou OK
     digitalWrite(pinoledvermelho, LOW);
     digitalWrite(pinoledverde, LOW);
   }
-  if(results.value == 0xAF548B7){ //Botao +Volume
-    Serial.println("Botao +Volume");
+  if(results.value == 0xAF548B7 || results.value == 0xFF6897){ //Botao +Volume ou 1
     digitalWrite(pinoledvermelho, HIGH); 
   }
-  if(results.value == 0xAF5A857){ //Botao -Volume
-    Serial.println("Botao -Volume");
+  if(results.value == 0xAF5A857 || results.value == 0xFF9867){ //Botao -Volume ou 2
     digitalWrite(pinoledvermelho, LOW);
   }
-  if(results.value == 0xAF59867){ //Botao +Canal
-    Serial.println("Botao +Canal");
+  if(results.value == 0xAF59867 || results.value == 0xFFB04F){ //Botao +Canal ou 3
     digitalWrite(pinoledverde, HIGH);
   }
-  if(results.value == 0xAF518E7){ //Botao -Canal
-    Serial.println("Botao -Canal");
+  if(results.value == 0xAF518E7 || results.value == 0xFF30CF){ //Botao -Canal ou 4
     digitalWrite(pinoledverde, LOW);
-  }
-  if(results.value == 0xFFFFFFF){ //Erro decode
-    Serial.println("Falha ao decodificar");
   }
   irrecv.resume(); //Le o próximo valor
 }
